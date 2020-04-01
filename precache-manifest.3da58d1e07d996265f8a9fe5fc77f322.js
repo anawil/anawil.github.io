8debbde7884ccadbeccdfe7a297da4b0f18cf7a1
-S'z- -•Gear'z•- <u5912087@au.edu>
2020-04-01 16:56:44 +0700

    React App

---
+++ precache-manifest.3da58d1e07d996265f8a9fe5fc77f322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdc3ee5ab9793fdbcfdd23458181529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a5683fb15c5de56fba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "817501ac7a3f1571ec03",
+    "url": "/static/js/2.d962924b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d962924b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a5683fb15c5de56fba",
+    "url": "/static/js/main.d421d7b6.chunk.js"
+  },
+  {
+    "revision": "5472d73486a8ceaf6e0b",
+    "url": "/static/js/runtime-main.0c73d137.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
